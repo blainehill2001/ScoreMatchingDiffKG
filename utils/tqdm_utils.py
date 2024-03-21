96c7d64edf4eb385a90bb4e4b53ab300bc3c9452
blainehill2001 <blainehill2001@gmail.com>
2024-03-20 23:56:42 -0500

    Added negative sampling

---
+++ utils/tqdm_utils.py
@@ -0,0 +1,9 @@
+import tqdm
+
+#TODO: get this to silence any tqdm inside some function
+def silence_tqdm(func):
+    def wrapper(*args, **kwargs):
+        with tqdm.tqdm(disable=True):
+            func_result = func(*args, **kwargs)
+        return func_result
+    return wrapper
