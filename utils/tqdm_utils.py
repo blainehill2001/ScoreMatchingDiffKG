fb2f5aa7937a593767b5682601238844692c802d
blainehill2001 <blainehill2001@gmail.com>
2024-04-16 12:54:26 -0500

    added requirements.txt using pipreqsnb

--- utils/tqdm_utils.py
+++ utils/tqdm_utils.py
@@ -1,9 +1,11 @@
-import tqdm
+import tqdm  # type: ignore
 
-#TODO: get this to silence any tqdm inside some function
+
+# TODO: get this to silence any tqdm inside some function
 def silence_tqdm(func):
     def wrapper(*args, **kwargs):
         with tqdm.tqdm(disable=True):
             func_result = func(*args, **kwargs)
         return func_result
+
     return wrapper
